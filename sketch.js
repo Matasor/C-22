9e7d61ede68171f563677a91da7c16db30fbd47d
Matasor <96345204+Matasor@users.noreply.github.com>
2022-03-22 16:51:15 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+var palyer, playerBase, playerArcher;
+var baseimage;
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.png");
+  baseimage = loadImage("./assets/base.png");
+  playerimage = loadImage("./assets/player.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  engine = Engine.create();
+  world = engine.world;
+  angleMode(DEGREES);
+
+  //criar corpo da base do jogador
+playerBase = Bodies.rectangle(200,350,180,150,options);
+World.add(world,playerBase)
+  //criar corpo do jogador
+  player = Bodies.rectangle(250,playerBase.y - 160,50,180,options);
+World.add(world,player)
+
+
+}
+
+function draw() {
+  background(backgroundImg);
+
+Engine.update(engine)
+Image(baseimage,playerBase.option.x,playerBase.options.y,180,150)
+Image(playerimage,player.option.x,player.options.y,50,180)
+
+ 
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("ARQUEIRO ÉPICO", width / 2, 100);
+}
